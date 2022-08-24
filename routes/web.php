efde9ec6597822e03c58c8929317fb12f4a194fa
CW-HoanPL <CW-HoanPL@github.com>
2022-08-24 15:12:02 +0700

    Routes and Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::resource('homes', HomeController::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/homes');
 });
